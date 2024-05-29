0dc39e393f0c93233a04c99b40fd1167342f1004
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-05-29 23:14:44 +0500

    testting

---
+++ js/index.js
@@ -0,0 +1,87 @@
+//  1- MISOL 
+
+// let = prompt(' 1 dan 50 gacha bolgan sonlar orasida toq sonlarning kvadratlarini konsolga chiqarish');
+// for(let i = 1; i <= 50; i++){
+//     if(i % 2 == 1){
+//         console.log(i**2);
+//     }
+// }
+
+// 2 - MISOL 
+
+// let = prompt(' 1 dan 10 gacha bolgan sonlarning kublarini konsolga chiqarish');
+// for(let i = 1; i <= 10; i++){
+//     console.log(i**3);
+// }
+
+
+// 3 - MISOL 
+
+// let = prompt(' 1 dan 20 gacha bolgan sonlarning faktorialini konsolga chiqarish');
+// let p = 1;
+// for(let i = 1; i <= 20; i++){
+//     p = p * i;
+//     console.log(p);
+// }
+
+
+
+// 4 - MISOL 
+
+// let = prompt('1 dan 30 gacha bolgan sonlarning kvadratlari yigindisini topish');
+// let p = 0;
+// for(let i = 1; i <= 30; i++){
+//     p += i**2;
+// } console.log(p);
+
+
+
+
+// 5 - MISOL 
+
+// for(let i = 1; i <= 20; i++){
+//     let counter = 0;
+//     for(let j = 1; j <= i; j++){
+//         if(i % j == 0){
+//             counter++;
+//         }
+//     }
+//     if(counter == 2) {
+//         console.log(i);
+//     }
+// }
+
+
+//  6 -MISOL 
+
+
+// let = prompt(' 1 dan 100 gacha bolgan sonlar orasida 2 ga kopaytirilganda 100 dan kichik boladigan sonlarni topish');
+// for(let i = 1; i < 100; i++) {
+//   if((i * 2) < 100){
+//     console.log(i);
+// }
+// }
+
+
+//  7 - MISOL 
+
+
+// let = prompt('1 dan 100 gacha bolgan sonlar orasida 7 ga bolinadigan va 5 ga bolinmaydigan sonlarni topish');
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 7 === 0 && i % 5 !== 0) {
+//         console.log(i); 
+//     }
+// }
+
+// 8 - MISOL 
+
+// let  = prompt('Berilgan sonning barcha boluvchilarini toping va ularning yigindisini hisoblang');
+// let n = +prompt('son kiriting');
+// let p = 0;
+// for (let i = 1; i <= n; i++) {
+//     if (n % i === 0) { 
+//         p += i;
+//     }
+// }
+// console.log(p);
+
